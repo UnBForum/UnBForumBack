8399ebfe4f00774649da72b9ccd8fef0eb9de0b1
Herick Portugues <herick.portugues@gmail.com>
2023-10-14 22:02:47 -0300

    Create dependency to verify token and get authenticated user

--- src/routes/deps.py
+++ src/routes/deps.py
@@ -1,4 +1,11 @@
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
 from src.db.connection import LocalSession
+from src.utils.jwt import verify_token_n_get_user
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl='/users/login')
 
 
 def get_db_session():
@@ -7,3 +14,10 @@ def get_db_session():
         yield session
     finally:
         session.close()
+
+
+def get_authenticated_user(
+    db_session: Session = Depends(get_db_session),
+    token: str = Depends(oauth_scheme)
+):
+    return verify_token_n_get_user(token, db_session)
--- src/utils/jwt.py
+++ src/utils/jwt.py
@@ -1,5 +1,9 @@
-from jose import jwt
+from jose import jwt, JWTError
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
 from decouple import config
+
+from src.db.models import User
 
 SECRET_KEY = config('SECRET_KEY')
 ALGORITHM = config('ALGORITHM')
@@ -8,3 +12,21 @@ ALGORITHM = config('ALGORITHM')
 def create_access_token(user_email: str):
     data = {"sub": user_email}
     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_token_n_get_user(token: str, db_session: Session):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token inválido",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        data = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        raise credentials_exception
+
+    user_email = data.get("sub")
+    user = db_session.query(User).filter_by(email=user_email).one_or_none()
+    if not user:
+        raise credentials_exception
+    return user
