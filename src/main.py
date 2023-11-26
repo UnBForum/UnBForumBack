b3afde60ac12fbec528f1588e348035f15e9f5da
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-11-26 20:56:35 -0300

    Fix cors error on API

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import auth_router, user_router, topic_router, category_router, comment_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
