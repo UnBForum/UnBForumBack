c4850cc9aca185a6cb4a6f8ccd9bb2fc4bf41ea0
Herick Portugues <herick.portugues@gmail.com>
2023-11-26 23:02:52 -0300

    Add filters to get_all_topics route

--- src/schemas/category.py
+++ src/schemas/category.py
@@ -1,7 +1,11 @@
 import re
 from datetime import datetime
+from typing import Optional, List
 
 from pydantic import BaseModel, field_validator
+from fastapi_filter.contrib.sqlalchemy import Filter
+
+from src.db.models import Category
 
 
 class CategoryCreateSchema(BaseModel):
@@ -22,3 +26,14 @@ class CategoryRetrieveSchema(BaseModel):
     color: str
     created_at: datetime
     updated_at: datetime
+
+
+class CategoryFilterSchema(Filter):
+    id: Optional[int] = None
+    id__in: Optional[List[int]] = None
+    name: Optional[str] = None
+    name__in: Optional[List[str]] = None
+
+
+    class Constants(Filter.Constants):
+        model = Category
--- src/schemas/topic.py
+++ src/schemas/topic.py
@@ -1,9 +1,14 @@
 from typing import List, Optional
 from datetime import datetime
 
-from pydantic import BaseModel
+from fastapi import Query
+from pydantic import BaseModel, Field
+from fastapi_filter import FilterDepends, with_prefix
+from fastapi_filter.contrib.sqlalchemy import Filter
 
-from src.schemas.category import CategoryRetrieveSchema
+from src.db.models import Topic
+from src.schemas.category import CategoryRetrieveSchema, CategoryFilterSchema
+from src.schemas.user import UserRetrieveSchema
 
 
 class TopicCreateSchema(BaseModel):
@@ -19,14 +24,24 @@ class TopicRetrieveSchema(BaseModel):
     title: str
     content: str
     is_fixed: bool
+    user: UserRetrieveSchema
     # files: List[File]
     categories: List[CategoryRetrieveSchema]
-    user_id: int
     created_at: datetime
     updated_at: datetime
-    # comments: List[CommentRetrieveSchema] = []
+    # comments: List[CommentRetrieveSchema]
 
 
 class TopicUpdateSchema(BaseModel):
     title: Optional[str] = None
     content: Optional[str] = None
+
+
+class TopicFilterSchema(Filter):
+    search: Optional[str] = Field(Query(None, description='Pesquisa por título ou conteúdo'))
+    order_by: Optional[List[str]] = ['-created_at']
+    category: Optional[CategoryFilterSchema] = FilterDepends(with_prefix('category', CategoryFilterSchema))
+
+    class Constants(Filter.Constants):
+        model = Topic
+        search_model_fields = ['title', 'content']
