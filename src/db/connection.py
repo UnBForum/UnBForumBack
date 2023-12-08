05852e61e8f80c6a145c3f01b5e259b74be30fb9
Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
2023-12-08 09:38:03 -0300

    Update connection.py

--- src/db/connection.py
+++ src/db/connection.py
@@ -2,7 +2,7 @@ from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = config('DATABASE_URL').replace('postgres://', 'postgresql://')
+DB_URL = config('DB_URL').replace('postgres://', 'postgresql://')
 
 Engine = create_engine(DB_URL, pool_pre_ping=True)
 LocalSession = sessionmaker(bind=Engine)
