f57a4279a327d77ddc03f81afc4f05a81c26a53a
Herick Portugues <herick.portugues@gmail.com>
2023-12-15 18:41:04 -0300

    Update test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose-test.yml build
 docker-compose -f docker-compose-test.yml up -d
 docker-compose -f docker-compose-test.yml exec -it app-test alembic upgrade head
-docker-compose -f docker-compose-test.yml exec -it app-test pytest .
+docker-compose -f docker-compose-test.yml exec -it app-test pytest --cov=src
